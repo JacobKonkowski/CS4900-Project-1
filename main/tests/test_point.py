896ebcaf915c3bff0ca45ffa7cc0aca7a4873975
Jake Konkowski <jrkonkowski-social@protonmail.com>
2022-11-01 16:52:41 -0400

    Created test_point.py tests

---
+++ main/tests/test_point.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+class TestPoint:
+
+    def test_get_coords(self):
+        point = Point()
+        assert point.get_coords() == [0,1,2,3,4]
+
+    def test_set_coords(self):
+        point = Point([4,3,2,1,0])
+        point.set_coords([0,1,2,3,4])
+
+        assert point.get_coords() == [0,1,2,3,4]
