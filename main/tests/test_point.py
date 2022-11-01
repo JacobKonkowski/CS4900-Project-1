37808c142a84ff7f820edb7e790d1c5f3706908d
Jake Konkowski <jrkonkowski-social@protonmail.com>
2022-11-01 17:06:28 -0400

    Added test for Point.set_coords() typing

--- main/tests/test_point.py
+++ main/tests/test_point.py
@@ -12,3 +12,8 @@ class TestPoint:
         point.set_coords([0,1,2,3,4])
 
         assert point.get_coords() == [0,1,2,3,4]
+
+    def test_set_coords_type(self):
+        with pytest.raises(TypeError):
+            point = Point()
+            point.set_coords("Donkey")
